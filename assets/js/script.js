883ac63a2fd9e6c9640c236cbcd8c500f81a081c
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-01-03 13:22:03 -0500

    finalized the 4 questions for code quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,28 @@
 var time = 5;
 var clockId;
+//Create the 4 questions for the quiz.
 var questions = [{
     q: 'What is the key word to create a variable?',
-    choices: ['HTML', 'CSS', 'var'],
+    choices: ['HTML', 'CSS', 'var', 'flexbox'],
     correct: 'var',
 },
 {
-    q: 'What is the key word to create a variable?',
-    choices: ['HTML', 'CSS', 'var'],
-    correct: 'var',
-}, {}, {}];
+    q: 'Which coding language is considered the foundation/structure of a webpage?',
+    choices: ['HTML', 'CSS', 'JavaScript', 'Python'],
+    correct: 'HTML',
+}, 
+{
+    q: 'A container object that holds a fixed number of values of a single type is?',
+    choices: ['array', 'object', 'string', 'boolean'],
+    correct: 'array',
+}, 
+{
+    q: 'Which primitive type is an expression that evaluates to be either true or false?',
+    choices: ['number', 'string', 'boolean', 'undefined'],
+    correct: 'boolean',
+}];
 
-//Iterate the array of quesions
+//Iterate the array of quesions.
 
 function startClock() {
     time--;
