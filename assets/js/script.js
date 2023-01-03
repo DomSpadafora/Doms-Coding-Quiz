8f59232567f4cb7330e867b9bf5583a9290bbd8c
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-01-03 10:06:01 -0500

    began formating the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,33 @@
-var time = 60;
+var time = 5;
 var clockId;
+var questions = [{
+    q: 'What is the key word to create a variable?',
+    choices: ['HTML', 'CSS', 'var'],
+    correct: 'var',
+},
+{
+    q: 'What is the key word to create a variable?',
+    choices: ['HTML', 'CSS', 'var'],
+    correct: 'var',
+}, {}, {}];
 
-function startClock(){
-    time --;
+//Iterate the array of quesions
 
-    if(time<1) {
+function startClock() {
+    time--;
+
+    if (time < 1) {
         clearInterval(clockId);
-        time=0;
+        time = 0;
     }
 
-    clock.innerHTML=time;  
+    clock.innerHTML = time;
 }
 
-const beginQuiz = ()=>{
-    console.log('Hello world!!!') ;
+const beginQuiz = () => {
+    console.log('Hello world!!!');
 
     clockId = setInterval(startClock, 1000);
 };
 
-start.addEventListener('click',beginQuiz)
+start.addEventListener('click', beginQuiz)
