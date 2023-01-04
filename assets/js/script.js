e4d6081dd0e38430d11ba42bb968576da7148d54
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-01-03 22:23:17 -0500

    begining code for end of quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var clockId;
 var time = 60;
 var score = 0;
 
-//need to stop time and setup high score section
+//need to stop time and setup high score
 
 const startClock = () => {
     time--;
@@ -16,15 +16,16 @@ const startClock = () => {
     clock.innerHTML = time;
 }
 
+//functionality when correct answer has been selected
 const handleCorrect = () => {
-    console.log('Correct!!!!')
-    score +=25;
+    console.log('Correct!!!!');
     correct.style.display = 'block'; 
     setTimeout(()=>correct.style.display = 'none', 1000)
     qI++;
     handleQuestions();
 }
 
+//functionality when incorrect answer has been selected
 const handleIncorrect = () => {
     console.log('Incorrect!!!')
     time -=10;
@@ -33,11 +34,12 @@ const handleIncorrect = () => {
     qI++;
     handleQuestions();
 }
-
+//create function for answering the questions
 const handleAnswers = answer => {
     answer==questions[qI].C ? handleCorrect() : handleIncorrect();
 };
 
+//create function for pulling in the questions to webpage
 const handleQuestions = () => {
     let { Q, A } = questions[qI];
     main.innerHTML = `<h1>${Q}</h1><div id="answers"></div>`;
@@ -47,11 +49,17 @@ const handleQuestions = () => {
     });
 };
 
+
+//If all questions have been answered and time is left on the clock.???
+const endQuiz = () => {
+    console.log('end of quiz')
+}
+
 const beginQuiz = () => {
-    
-    
+
     handleQuestions();
     clockId = setInterval(startClock, 1000);
 };
 
+//adding event listener
 start.addEventListener('click', beginQuiz)
